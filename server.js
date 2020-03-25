2aac108e63f85bf8bd76eb1cc0e52b5525e8e36a
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-25 13:17:21 +1100

    adding the middleware

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 // instance of the express
 const app = express();
 
+//Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
 // variable for port
 const port = process.env.PORT || 3000;
 
