6a20f972c74676053ed0fe068884c3fc3a846627
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-26 12:17:15 +1100

    adding the database connection to the server js file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// requrie database config file
+const databaseconnection = require('./config/database.config.js');
 
 // instance of the express
 const app = express();
@@ -14,6 +17,14 @@ app.use('/users', require('./app/routes/users.routes.js'));
 
 // variable for port
 const port = process.env.PORT || 3000;
+
+//database connection
+mongoose.connect(databaseconnection.url,{
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log("Could not connect to the database");
+});
 
 //starting the server
 app.listen(port, ()=>{
