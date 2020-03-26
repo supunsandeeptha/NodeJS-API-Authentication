bfb19e295bff382aa7df7986bb9894669e855e36
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-26 11:52:41 +1100

    adding the router helper to the routes file

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = require('express-promise-router')();
+//adding the route helper
+const {validateBody, schemas } = require('../helpers/routerHelpers.js');
 // controller 
 const UsersController = require('../controllers/users.controllers.js');
 
-// Signup Route
+// Signup Route wit validation
 router.route('/signup')
-.post(UsersController.signUp);
+.post(validateBody(schemas.authSchema),UsersController.signUp);
 
 // Signin Route
 router.route('/signin')
