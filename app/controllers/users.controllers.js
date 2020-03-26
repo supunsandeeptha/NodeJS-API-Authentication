013ad347c381a7c780221c41f52d5d01d47f9908
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-26 19:12:36 +1100

    adding the Json Web Token Signing to SignUp method

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -1,11 +1,22 @@
+const JWT = require('jsonwebtoken');
 //importing the usermodel
 const userModel = require('../models/user.model.js');
+//importing the Json Web Token secret
+const JWT_SECRET = require('../../config/token.config.js');
 
+// Json Web Token generation as a function
+signToken = user => {
+    return JWT.sign({
+        iss: 'supunsandeeptha',
+        sub: user.id,
+        iat:new Date().getTime(), // current time
+        exp: new Date().setDate(new Date().getDate() + 1) //current time + 1 day ahead
+    },JWT_SECRET.JSON_SECRET);
+}
 module.exports ={
     //SignUp Controller
     signUp: async(req, res, next) =>{
-        console.log('Sign up function has been called !');
-
+        // constants for email and password
         const email = req.value.body.email;
         const password = req.value.body.password;
         //check for a existing user 
@@ -24,8 +35,12 @@ module.exports ={
         // using await since it will take some time to create the user
         await newUser.save();
 
-        //sending a response to the cline
-        res.json({user: 'created'});
+        
+        // Signing the Json Web Token Payload Section
+        const token = signToken(newUser);
+
+        //sending a response to the client with a token
+        res.status(200).json({token:token});
     },
 
     // SignIn Controller
